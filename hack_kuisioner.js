320156ca8d5e77b78a107179d1920f9b39409cc0
Tian113654 <112967402+Tian113654@users.noreply.github.com>
2022-12-07 11:26:04 +0700

    Update hack_kuisioner.js

--- hack_kuisioner.js
+++ hack_kuisioner.js
@@ -1,8 +1,10 @@
-function hack() {
-    var x = document.querySelectorAll('input[value="4"]');
-    var btnX = document.getElementById('Simpan');
-    for(let i = 0; i < x.length; i++) {
-        x[i].checked = true;
+(
+    function hack() {
+        var x = document.querySelectorAll('input[value="4"]');
+        var btnX = document.getElementById('Simpan');
+        for(let i = 0; i < x.length; i++) {
+            x[i].checked = true;
+        }
+        btnX.click();
     }
-    btnX.click();
-}
+);
